e969e4ca0df9984e86efee52c51ec2ed4f73eef1
Wendell Diniz <wendellcien@gmail.com>
2022-05-07 23:40:15 -0300

    Config next routing.

--- src/api/load-pages.js
+++ src/api/load-pages.js
@@ -5,23 +5,40 @@ import { mapData } from '../api/map-data';
 export const loadPages = async (slug = '') => {
   const cleanSlug = slug ? slug.replace(/[^a-z0-9-_]/gi) : '';
   // const slug = config.defaultSlug;
-  const query = qs.stringify(
-    {
-      filters: { slug: cleanSlug },
-      populate: [
-        'menu',
-        'menu.logo',
-        'menu.menu_link',
-        'sections',
-        'sections.image',
-        'sections.image_grid',
-        'sections.image_grid.image',
-        'sections.text_grid',
-        'sections.metadata',
-      ],
-    },
-    { encodeValuesOnly: true },
-  );
+  const query = slug
+    ? qs.stringify(
+        {
+          filters: { slug: cleanSlug },
+          populate: [
+            'menu',
+            'menu.logo',
+            'menu.menu_link',
+            'sections',
+            'sections.image',
+            'sections.image_grid',
+            'sections.image_grid.image',
+            'sections.text_grid',
+            'sections.metadata',
+          ],
+        },
+        { encodeValuesOnly: true },
+      )
+    : qs.stringify(
+        {
+          populate: [
+            'menu',
+            'menu.logo',
+            'menu.menu_link',
+            'sections',
+            'sections.image',
+            'sections.image_grid',
+            'sections.image_grid.image',
+            'sections.text_grid',
+            'sections.metadata',
+          ],
+        },
+        { encodeValuesOnly: true },
+      );
   const url = `${config.url}/pages?`;
   const raw = await fetch(url + query);
   const json = await raw.json();
